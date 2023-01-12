bdfc901ae5c18706579d35309378ff3dbeb6c4a1
Hiram Hernandez Garcia (Externo) <e-hehernandez@compartamos.com>
2023-01-12 10:38:50 -0600

    componetes de lista y de tarjeta para desplegar la información de los proyectos

--- react/keeptrack/src/projects/ProjectsPage.tsx
+++ react/keeptrack/src/projects/ProjectsPage.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { MOCK_PROJECT } from './MockProjects';
+import ProjectList from './ProjectList';
 
 export default function ProjectsPage() {
   return (
-    <h1>Projects</h1>
+    <>
+      <h1>Projects desde otro componente</h1>
+      <ProjectList projects={MOCK_PROJECT}/>
+    </>
+    
   )
 }
